7ceab65fda2eb49ed1f21be913fb131ff8b0da2e
Brandon McInerney <bj.mcinerney@hotmail.com.au>
2019-08-19 10:51:52 +1000

    Create trafficlight.html

---
+++ u3192633/Week 3 Tutorial/trafficLight.js
@@ -0,0 +1,21 @@
+var road1 = "red";
+var road2 = "green";
+var pressurePlateRoad1 = false;
+var pressurePlateRoad2 = false;
+
+do {
+  road1 = "green";
+  setTimeout(120000);
+} while ((pressurePlateRoad2 = false));
+
+if (pressurePlateRoad2 == true && pressurePlateRoad1 == false) {
+  road1 = "orange";
+  setTimeout(5000);
+  road1 = "red";
+}
+
+if (pressurePlateRoad1 == true && pressurePlateRoad2 == false) {
+  road2 = "orange";
+  setTimeout(5000);
+  road2 = "red";
+}
