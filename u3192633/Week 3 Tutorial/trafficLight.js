a0d32b78ca113fadcec3409c2d3158c4036f4f7f
Brandon McInerney <bj.mcinerney@hotmail.com.au>
2019-08-19 11:11:38 +1000

    Updates to File

--- u3192633/Week 3 Tutorial/trafficLight.js
+++ u3192633/Week 3 Tutorial/trafficLight.js
@@ -3,19 +3,21 @@ var road2 = "green";
 var pressurePlateRoad1 = false;
 var pressurePlateRoad2 = false;
 
-do {
-  road1 = "green";
-  setTimeout(120000);
-} while ((pressurePlateRoad2 = false));
+function trafficLight() {
+  do {
+    road1 = "green";
+    setTimeout(120000);
+  } while ((pressurePlateRoad2 = false));
 
-if (pressurePlateRoad2 == true && pressurePlateRoad1 == false) {
-  road1 = "orange";
-  setTimeout(5000);
-  road1 = "red";
-}
+  if (pressurePlateRoad2 == true && pressurePlateRoad1 == false) {
+    road1 = "orange";
+    setTimeout(5000);
+    road1 = "red";
+  }
 
-if (pressurePlateRoad1 == true && pressurePlateRoad2 == false) {
-  road2 = "orange";
-  setTimeout(5000);
-  road2 = "red";
+  if (pressurePlateRoad1 == true && pressurePlateRoad2 == false) {
+    road2 = "orange";
+    setTimeout(5000);
+    road2 = "red";
+  }
 }
