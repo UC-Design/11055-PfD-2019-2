16adff0678d3ae9308b66ae5786a802aac6554d2
Lachlan Murray <pyrofuzion@gmail.com>
2019-08-26 09:31:58 +1000

    Update trafficLight.js

--- u3190538/Tutorial Activities/Week 3/trafficLight.js
+++ u3190538/Tutorial Activities/Week 3/trafficLight.js
@@ -1,21 +1,30 @@
-var road1 = "red";
-var road2 = "red";
-var pressurePadRoad1;
-var pressurePadRoad2;
+var road1 = {speed = 80, sensor, lights};
+var road2 = {speed = 60, sensor, lights};
 
-do {
-  road1 = "green";
-  setTimeout(120000);
-} while (pressurePadRoad1 == false);
+var green = "green";
+var amber = "amber";
+var red = "red";
 
-if (pressurePadRoad2 == true && pressurePadRoad1 == false) {
-  road1 = "amber";
-  setTimeout(5000);
-  road1 = "red";
-}
+function trafficLights() {
+    if (road1.sensor == true && road2.sensor == false) {
+        road2.lights = amber;
+        setTimeout(road1.speed*100);
+        road2.lights = red;
+        setTimeout(3000);
+        road1.lights = green;
+    } else {
+        road1.lights = red;
+        road2.lights = green;
+    }
 
-if (pressurePadRoad1 == true && pressurePadRoad2 == false) {
-  road2 = "amber";
-  setTimeout(5000);
-  road2 = "red";
+    if (road2.sensor == true && road1.sensor == false) {
+        road1.lights = amber;
+        setTimeout(road2.speed*100);
+        road1.lights = red;
+        setTimeout(3000);
+        road2.lights = green;
+    } else {
+        road1.lights = green;
+        road2.lights = red;
+    }
 }
