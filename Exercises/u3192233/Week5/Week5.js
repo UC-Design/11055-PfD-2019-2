26c636046b8691b33da8a0f617625ee41041413c
Morgan <morgan@wilde.mx>
2019-09-02 10:42:12 +1000

    Update Week5.js

---
+++ Exercises/u3192233/Week5/Week5.js
@@ -0,0 +1,20 @@
+// setup drawing canvas for p5.js
+function setup() {
+    createCanvas(720, 480);
+}
+// drawing items
+function draw() {
+    ellipse(50, 50, 80, 80);
+    line(1, 1, 100, 100);
+    
+// create if statement for mouse press
+    if (mouseIsPressed) {
+        rect(1, 1, 100, 100);
+        stroke('black');
+//        strokeWeight(10);
+    } else {
+// makes all strokes red from the beginning
+        stroke('red');
+    }
+    line(mouseX, mouseY, 120, 200);
+}
