6f1e7e1537ed7550df14ef5c7fda0e07b5e632dc
Morgan <morgan@wilde.mx>
2019-09-09 10:59:51 +1000

    Attempt at drawing a clock
    Circle with half of the hour marks done

--- Exercises/u3192233/Week6/Week6.js
+++ Exercises/u3192233/Week6/Week6.js
@@ -1,30 +1,38 @@
 // setup drawing canvas for p5.js
 function setup() {
-    createCanvas(360, 720);
+    createCanvas(720, 720);
 }
 // drawing items
 function draw() {
-    background(0);
-    stroke(255);
-    strokeWeight(4);
-    circle(180, 360, 340);
+    background(230);
+    circle(360, 360, 710);
+// 12
+    line(360, 10, 360, 50);
+// 1
+    line(530, 53, 505, 95);
+// 2
+    line(650, 160, 620, 190);
+// 3
+    line(705, 360, 665, 360);
+// 4
+    line(650, 550, 620, 520);
+// 5
+    line(530, 667, 505, 630);
+// 6
+    line(360, 705, 360, 665);
+// 7
+    line(650, 550, 620, 520);
+// 8
+    line(650, 550, 620, 520);
+// 9
+    line(650, 550, 620, 520);
+// 10
+    line(650, 550, 620, 520);
+// 11
+    line(650, 550, 620, 520);
+// minute hand
+    
 }
-// creating variable for light colour
-var light = 'red'
 function mouseClicked(){
-// switch case to rotate through light
-    switch(light){
-        case 'red':
-            fill('green');
-            light = 'green';
-            break;
-        case 'yellow':
-            fill('red');
-            light = 'red';
-            break;
-        case 'green':
-            fill('yellow');
-            light = 'yellow';
-            break;
-    }
+
 }
