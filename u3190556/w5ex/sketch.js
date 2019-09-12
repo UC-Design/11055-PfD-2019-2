6b994490bfde78bb4191eea9e39fce19e8d1ef43
sallym1m <53727988+sallym1m@users.noreply.github.com>
2019-09-12 19:26:31 +1000

    Update sketch.js

--- u3190556/w5ex/sketch.js
+++ u3190556/w5ex/sketch.js
@@ -2,11 +2,19 @@ function setup(){
     createCanvas(640,480);
 }
 function draw(){
-    if (mouseIsPressed){
+    if (mouseIsPressed) {
         fill(0);
     }else{
-        fill(225);
+        fill(326);
     }
     ellipse(mouseX, mouseY,80, 80);
 
+}
+function draw(){
+    if (mouseIsPressed){
+        strokeWeight(4);
+    }else{
+        strokeWeight(0);
+    }
+    stroke(350)
 }
