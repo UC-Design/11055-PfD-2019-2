082c93f642e0a0b6cdb1f8c914cbc19915226134
jordyn <jordyn.kerrison@live.com.au>
2019-09-15 09:52:26 +1000

    Updated sketch.js

--- Jordyn Kerrison/js/sketch.js
+++ Jordyn Kerrison/js/sketch.js
@@ -1,18 +1,37 @@
-
 var c = document.getElementById("mycanvas");
 var ctx = c.getContext ("2d");
     
-//rectangles
-rect(0,0,150,75, "green");
-rect(50,50,150,75, "red");
-rect(100,100,150,75, "yellow");
-    
-    function rect (x,y,width,height, color){
-    ctx.fillstyle = color;
-    ctx.fillRect(x,y,width,height, color);
-    }
-    
-//circle
+
+//face
 ctx.beginPath();
-ctx.arc(390,200,100, 0.4 * Math.PI, 1.5 * Math.PI);
+ctx.arc(390,200,100,0, 2 * Math.PI);
 ctx.stroke();
+
+
+
+
+//eyes
+ctx.beginPath();
+ctx.arc(355,175,10,0, 2 * Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.arc(425,175,10,0, 2 * Math.PI);
+ctx.stroke();
+
+
+//nose
+
+ctx.beginPath();
+ctx.fillRect(200,300,50,50);
+ctx.stroke();
+
+
+
+
+
+//mouth
+ctx.beginPath();
+ctx.arc(390,205,60,0, 1 * Math.PI);
+ctx.stroke();
+
