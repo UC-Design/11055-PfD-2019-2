f4ca118cb5740b2dc30fe6fb77ec3cbcd7404c85
Morgan <morgan@wilde.mx>
2019-09-16 09:39:51 +1000

    Substantial change to Project1
    Changing flow, adding functionality, removing specific design choices

--- Exercises/u3192233/Project 1/Project1.js
+++ Exercises/u3192233/Project 1/Project1.js
@@ -1,91 +1,65 @@
 // setup variables
+let bgvalue = 230
+let myName = ['Morgan', 'MORGAN', 'MoRgAn', 'Nagrom']
+let i = 0;
 
 // setup drawing canvas for p5.js
 function setup() {
     createCanvas(720, 720);
 }
+
 // drawing items
 function draw() {
-    background(230);
-// draw smile
+    background(bgvalue);
+    // mouse controlled yellow circle used to highlight
+    noStroke();
+    fill('gold');
+    strokeWeight(6);
+    circle(mouseX, mouseY, 400);
+    // invisible text
+    noStroke();
+    fill(230);
+    textSize(50);
+    text(myName[i], 250, 80);
+    // draw invisible head
+    stroke(230);
+    circle(360, 270, 300);
+    // draw smile
     stroke('black');
     fill('white');
     strokeWeight(2);
     arc(340, 340, 80, 80, 0, PI + QUARTER_PI, CHORD);
-// draw eyes
+    // draw eyes
     stroke('white');
     ellipse(300, 270, 80, 60);
     ellipse(420, 270, 80, 60);
-    stroke('black');
+    stroke('green');
     strokeWeight(30);
     point(300, 270);
     point(420, 270);
-// draw eyebrows
-    stroke('black');
+    // draw eyebrows
+    stroke('maroon');
     strokeWeight(6);
     noFill();
     arc(295, 240, 100, 80, PI + QUARTER_PI, TWO_PI);
     arc(420, 240, 100, 80, PI, TWO_PI - QUARTER_PI);
-// artistic outline of head
-    stroke('red');
-    strokeWeight(6);
-    line(0, 0, 40, 720);
-    line(0, 0, 80, 720);
-    line(0, 0, 120, 720);
-    line(0, 0, 160, 720);
-    line(0, 0, 200, 720);
-    line(0, 0, 240, 720);
-    line(0, 0, 280, 720);
-    line(0, 0, 320, 720);
-    line(0, 0, 360, 720);
-    line(0, 0, 400, 720);
-    line(0, 0, 200, 330);
-    line(0, 0, 190, 280);
-    line(0, 0, 190, 240);
-    line(0, 0, 190, 200);
-    line(0, 0, 200, 180);
-    line(0, 0, 220, 160);
-    line(0, 0, 240, 140);
-    line(0, 0, 260, 135);
-    line(0, 0, 280, 130);
-    line(0, 0, 300, 130);
-    line(0, 0, 340, 130);
-    line(0, 0, 380, 130);
-    line(0, 0, 420, 130);
-    line(0, 0, 720, 180);
-    line(0, 0, 720, 140);
-    line(0, 0, 720, 100);
-    line(0, 0, 720, 60);
-    line(0, 0, 720, 20);
-    line(720, 0, 640, 720);
-    line(720, 0, 600, 720);
-    line(720, 0, 560, 720);
-    line(720, 0, 520, 720);
-    line(720, 0, 480, 720);
-    line(720, 0, 440, 720);
-    line(720, 0, 400, 720);
-    line(720, 0, 360, 720);
-    line(720, 0, 320, 720);
-    line(720, 0, 520, 330);
-    line(720, 0, 530, 280);
-    line(720, 0, 530, 240);
-    line(720, 0, 530, 200);
-    line(720, 0, 520, 180);
-    line(720, 0, 500, 160);
-    line(720, 0, 480, 140);
-    line(720, 0, 460, 135);
-    line(720, 0, 440, 130);
-    line(720, 0, 420, 130);
-    line(720, 0, 380, 130);
-    line(720, 0, 340, 130);
-    line(720, 0, 0, 180);
-    line(720, 0, 0, 140);
-    line(720, 0, 0, 100);
-    line(720, 0, 0, 60);
-    line(720, 0, 0, 20);
-// draw body with a see through shirt
+    // draw body with a see through shirt
     stroke('white');
     let c = color(0, 126, 255, 200);
     fill(c);
     rect(100, 480, 500, 360, 30);
+}
+
+function mouseClicked(){
+    if (bgvalue === 230){
+        bgvalue = 0;
+        if (i < myName.length - 1){
+            i++;
+        } else{
+            i = 0;
+        }
+    } else{
+        bgvalue = 230;
+    }
+        
 }
