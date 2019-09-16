a4274ed318b3646ad48a5f5429293613e8ea7aef
Katie <katie.rae.owen@hotmail.com>
2019-09-16 14:14:09 +1000

    committing stuff

---
+++ Exercises/Katie Owen/katie_scriptwk5.js
@@ -0,0 +1,16 @@
+document.write("hi");
+
+function setup (){
+    createCanvas(640, 480)
+
+}
+
+function draw (){
+    if (mouseIsPressed) {
+        fill(0);
+    } else {
+        fill(255);
+    }
+
+    ellipse(mouseX, mouseY, 50, 80);
+}
