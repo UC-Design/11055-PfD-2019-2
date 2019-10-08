f9ed0781783b6b870faf7e2c164edb501acf9862
voilamitrev <54016355+voilamitrev@users.noreply.github.com>
2019-10-08 11:57:45 +1100

    changes

---
+++ u3187849/Python Exercises/firstPythonProject.py
@@ -0,0 +1,17 @@
+# This is my first python program
+# This is how one writes a comment
+"""
+    This is a multiline comment
+"""
+# This how you print
+print("hello world")
+# This is a variable that contains the int 19
+age = 19
+print("Sup, my name is Matej and I am " + str(age) + " years old.")
+# Write to file
+fileWrite
+# Reading from text file
+fileRead = open("New Text Document.txt", "r")
+if fileRead.mode == 'r':
+    content = fileRead.read()
+print(content)
