176a7bb87cc747e89843ab7c47fa51197b71c2f0
rafir8282 <54016403+rafir8282@users.noreply.github.com>
2019-10-15 03:52:05 +1100

    p2-update-v1.0

--- Project2/u3191010/BookIdentifier/BookIdentifier/BookIdentifier.py
+++ Project2/u3191010/BookIdentifier/BookIdentifier/BookIdentifier.py
@@ -1,23 +1,27 @@
+# Necessary imports
 import os.path
 import sys
 import webbrowser
 
+# Boolean used to loop menu
 menu_loop = True
 
 while menu_loop == True:
-    print("Welcome to Book Identifier. Add ISBN numbers to a text file and we will show you the corresponding books.")
-    
+    # Menu
+    print("I am the Book Identifier. Add ISBN's to a text file and I can find and show you the corresponding books.")
     print("1 - Find Books")
     print("2 - Instructions")
     print("3 - Quit\n")
 
     try:
         menu_choice = int(input())
-
+    
+    # Exception for non-integer inputs
     except:
         continue
 
     if menu_choice == 1:
+        # Boolean used to loop filename input when an invalid input is entered
         input_loop = True
         
         while input_loop == True:
@@ -25,19 +29,25 @@ while menu_loop == True:
             file_name = input()
             print()
             
+            # Check if file exists
             if os.path.exists(file_name) == True:
+                # Read file
                 open_file = open(file_name)
                 read_file = open_file.readlines()
 
+                # Create webpage
                 html_file = open("ISBN.html", "w+")
 
                 html_file.write("<html><head><title>Your Books</title><link rel=\"stylesheet\" type=\"text/css\" href=\"Style.css\" /></head><body><h1>Your Books</h1>")
 
                 for line in read_file:
+                    # Call API and print book cover to webpage
                     html_file.write("<div class=\"background\"></div><div class=\"bookdisplay\"><h3>" + line + "</h3><img src=\"http://covers.openlibrary.org/b/isbn/" + line + "-L.jpg\" /></div>")
 
+                
                 html_file.write("</body></html>")
 
+                # Open webpage in a web browser
                 webbrowser.open("ISBN.html", new = 2)
 
                 open_file.close()
@@ -49,11 +59,11 @@ while menu_loop == True:
                 continue
 
     elif menu_choice == 2:
-        print("\nTo enter ISBN's as input, you must type up the ISBN's in a text file IN SEPARATE LINES and link to that text file appropriately. Have fun.\n")
+        print("\nTo enter different ISBN's as input, you must type up the ISBN's in a text file IN SEPARATE LINES and link to that text file appropriately. Have fun.\n")
     
     elif menu_choice == 3:
         print("\nGoodbye.")
-        sys.exit()
+        sys.exit() # Exit program
 
     else:
         continue
