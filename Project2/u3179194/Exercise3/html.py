816ee0e5d9f7147014a459b5c5a9fc01090b591e
kimbah <kim8hughes@iinet.net.au>
2019-10-14 17:00:58 +0800

    added heading content to f string as a variable

--- Project2/u3179194/Exercise3/html.py
+++ Project2/u3179194/Exercise3/html.py
@@ -5,7 +5,7 @@ html_output += f'<html>'
 html_output += f'\n\t<head>\n'
 html_output += f'\n\t</head>\n'
 html_output += f'\n\t<body>'
-html_output += f'\n\t\t<h1>Hello World</h1>'
+html_output += f'\n\t\t<h1>{heading_content}</h1>'
 html_output += f'\n\t</body>'
 html_output += f'\n</html>'
 
