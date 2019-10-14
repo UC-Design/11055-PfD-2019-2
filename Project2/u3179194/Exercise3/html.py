2dac625fd0df530df41e73edc68487f56d10b980
kimbah <kim8hughes@iinet.net.au>
2019-10-14 16:50:50 +0800

    Added first steps in Exercise3

---
+++ Project2/u3179194/Exercise3/html.py
@@ -0,0 +1,12 @@
+html_output = ''
+heading_content = 'Hello World!'
+
+html_output += f'<html>'
+html_output += f'\n\t<head>\n'
+html_output += f'\n\t</head>\n'
+html_output += f'\n\t<body>'
+html_output += f'\n\t\t<h1>Hello World</h1>'
+html_output += f'\n\t</body>'
+html_output += f'\n</html>'
+
+print(html_output)
