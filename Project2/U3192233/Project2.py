2d229bb434bf52441e2b5d946b96153fc2167612
Morgan <morgan@wilde.mx>
2019-10-21 11:34:38 +1100

    Updating P2
    Finished coding and html creation
    Finished error testing
    May require CSS

--- Project2/U3192233/Project2.py
+++ Project2/U3192233/Project2.py
@@ -1,46 +1,88 @@
-# Define variables
+#define variables
 import urllib.request
 import json
-import textwrap
 n = 0
 base_api_link = "https://www.googleapis.com/books/v1/volumes?q=isbn:"
-# Access ISBN file
-with open("ISBN.txt") as ISBNfile:
-    content = ISBNfile.readlines()
-# FOR content array length
-for x in content:
-    with urllib.request.urlopen(base_api_link + content[n]) as apiurl:
-        text = apiurl.read()
-    # Decode the webpage
-    decoded_text = text.decode("utf-8")
-    obj = json.loads(decoded_text)
-    # Store book details
-    volume_info = obj["items"][0]
-    authors = obj["items"][0]["volumeInfo"]["authors"]
-    # displays title, summary, author, domain, page count and language
-    
-    print("\nTitle:", volume_info["volumeInfo"]["title"])
-    print("\nSummary:\n", textwrap.fill(volume_info["searchInfo"]["textSnippet"]))
-    n += 1
+bookCoverURL = "http://covers.openlibrary.org/b/isbn/"
+try:
+    #access ISBN file
+    with open("ISBN.txt") as ISBNfile:
+        content = ISBNfile.readlines()
+    #create/open file as overwrite
+    htmlFile = open("ISBNwebpage.html","w")
+    #initial html settings
+    htmlFile.write("""
+        <!doctype html>
+        <html>
+        <head>
+        <meta charset="utf-8">
+        <title>ISBN Lookup</title>
+        <meta name="viewport" content="width=device-width, initial-scale = 1.0, user-scalable = yes">
+        <link rel="stylesheet" href="005-MathsWebsite.css">
+        </head>
+        <body>
+        """)
+    # FOR content array length
+    for x in content:
+        try:
+            with urllib.request.urlopen(base_api_link + content[n]) as apiurl:
+                text = apiurl.read()
+            # Decode the webpage
+            decoded_text = text.decode("utf-8")
+            obj = json.loads(decoded_text)
+            # Store book details
+            volume_info = obj["items"][0]
+            authors = obj["items"][0]["volumeInfo"]["authors"]
+            #page's book number
+            htmlFile.write("<h1>Book " + str(n + 1) + "</h1><br>")
+            #page's book cover
+            htmlFile.write("<img src='" + bookCoverURL + content[n] + ".jpg' alt='Book Cover'>" + "<br>")
+            #page's book title
+            htmlFile.write("<h2>" + str(volume_info["volumeInfo"]["title"]) + "</h2><br>")
+            #page's author
+            htmlFile.write("Author(s): " + str(authors) + "<br>")
+            #page's book summary
+            htmlFile.write("Summary:<br>" + str(volume_info["searchInfo"]["textSnippet"]) + "<br>")
+            n += 1
+        except:
+            htmlFile.write("Sorry the ISBN entered in line " + str(n + 1) + "of the ISBN file does not exist")
+            n += 1
+
+    htmlFile.write("</body>")
+    htmlFile.close()
     ISBNfile.close()
+except:
+    #create/open file as overwrite
+    htmlFile = open("ISBNwebpage.html","w")
+    #initial html settings
+    htmlFile.write("""
+        <!doctype html>
+        <html>
+        <head>
+        <meta charset="utf-8">
+        <title>ISBN Lookup</title>
+        <meta name="viewport" content="width=device-width, initial-scale = 1.0, user-scalable = yes">
+        <link rel="stylesheet" href="005-MathsWebsite.css">
+        </head>
+        <body>
+        Sorry the ISBN file does not exist.<br>
+        Please ensure the ISBN.txt file is in the same folder as this script
+        </body>""")
+    htmlFile.close()
 
 #CHECK IF THERE ARE NUMBER
 #CHECK IF NUMBERS ARE VALID
 
 
 # Create CSS file
-# Create HTML file
-if n = 0:
-    #create html
-else:
-    #append html
-htmlContent = """
+#
+#    #create html
+#
+#    #append html
+#htmlContent = """
+#
+#"""
 
-"""
-
-htmlFile = open("ISBNwebpage.html","w")
-htmlFile.write(htmlContent)
-htmlFile.close()
 # Open HTML file
 
 
