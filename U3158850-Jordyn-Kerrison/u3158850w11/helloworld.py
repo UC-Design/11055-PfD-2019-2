07b9e4265c853c8c8b390104b69474853a7235af
jordyn <jordyn.kerrison@live.com.au>
2019-10-24 20:38:25 +1100

    Update helloworld.py

--- U3158850-Jordyn-Kerrison/u3158850w11/helloworld.py
+++ U3158850-Jordyn-Kerrison/u3158850w11/helloworld.py
@@ -1,1 +1,18 @@
-print("Hello World");
+# first python program
+# to write a ccomment use a hash
+"""
+    multiline comment
+"""
+# print function
+print("hello world")
+#variable ccontaining int 21
+age = 21
+print("My name is Jordyn and I am" + str(age) + " years of age.")
+# write to the file
+fileWrite
+# read from the file
+fileRead = open("text document.txt", "r")
+# if statement to read the file
+if fileRead.mode == 'r':
+    content = fileRead.read()
+print(content)
