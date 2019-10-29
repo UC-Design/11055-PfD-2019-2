649e9c6546c4dc69286e7c03b4592d09d085fce2
Morgan <morgan@wilde.mx>
2019-10-29 17:38:37 +1100

    Updated and created files for Project 2
    Updated Project2.py to final stage
    Created polished flow chart and Psuedo code

--- Project2/U3192233/Project2.py
+++ Project2/U3192233/Project2.py
@@ -1,14 +1,27 @@
 #define variables
 import urllib.request
 import json
-n = 0
+contentNo = 0
+authorNo = 0
 base_api_link = "https://www.googleapis.com/books/v1/volumes?q=isbn:"
 bookCoverURL = "http://covers.openlibrary.org/b/isbn/"
+
+#create css file
+cssFile = open("CSS.css","w")
+cssFile.write("""
+    body {background-image: linear-gradient(to bottom right, white, lightblue);}
+    h1{font-family: 'Liu Jian Mao Cao', cursive; font-size: 50px;}
+    h2{font-family: 'Liu Jian Mao Cao', cursive; font-size: 30px;}
+    p{font-family: 'Roboto', sans-serif;}
+    """)
+cssFile.close()
+
+#error checking ISBN file
 try:
     #access ISBN file
     with open("ISBN.txt") as ISBNfile:
         content = ISBNfile.readlines()
-    #create/open file as overwrite
+    #creating/overwriting HTML file
     htmlFile = open("ISBNwebpage.html","w")
     #initial html settings
     htmlFile.write("""
@@ -18,43 +31,53 @@ try:
         <meta charset="utf-8">
         <title>ISBN Lookup</title>
         <meta name="viewport" content="width=device-width, initial-scale = 1.0, user-scalable = yes">
-        <link rel="stylesheet" href="005-MathsWebsite.css">
+        <link rel="stylesheet" href="CSS.css">
+        <style>@import url('https://fonts.googleapis.com/css?family=Liu+Jian+Mao+Cao|Roboto&display=swap');
+        </style>
         </head>
         <body>
         """)
-    # FOR content array length
+    #FOR content loop for array length
     for x in content:
+        #error checking the ISBN
         try:
-            with urllib.request.urlopen(base_api_link + content[n]) as apiurl:
+            #creating URL to each ISBN number
+            with urllib.request.urlopen(base_api_link + content[contentNo]) as apiurl:
                 text = apiurl.read()
-            # Decode the webpage
+            #Decode the webpage to utf-8
             decoded_text = text.decode("utf-8")
             obj = json.loads(decoded_text)
-            # Store book details
+            #Store book details
             volume_info = obj["items"][0]
             authors = obj["items"][0]["volumeInfo"]["authors"]
-            #page's book number
-            htmlFile.write("<h1>Book " + str(n + 1) + "</h1><br>")
-            #page's book cover
-            htmlFile.write("<img src='" + bookCoverURL + content[n] + ".jpg' alt='Book Cover'>" + "<br>")
-            #page's book title
+            #write number on the ISBN list
+            htmlFile.write("<h1>Book " + str(contentNo + 1) + "</h1><br>")
+            #write the book's cover image
+            htmlFile.write("<img src='" + bookCoverURL + content[contentNo] + ".jpg' alt='Book Cover'>" + "<br>")
+            #write the book's title
             htmlFile.write("<h2>" + str(volume_info["volumeInfo"]["title"]) + "</h2><br>")
-            #page's author
-            htmlFile.write("Author(s): " + str(authors) + "<br>")
-            #page's book summary
-            htmlFile.write("Summary:<br>" + str(volume_info["searchInfo"]["textSnippet"]) + "<br>")
-            n += 1
+            #write the book's author
+            htmlFile.write("<p>Author(s): ")
+            for x in authors:
+                htmlFile.write(str(authors[authorNo]) + ". ")
+                authorNo += 1
+            htmlFile.write("<br>")
+            #write the book's summary
+            htmlFile.write("Summary:<br>" + str(volume_info["searchInfo"]["textSnippet"]) + "</p><br>")
+            authorNo = 0
+            contentNo += 1
         except:
-            htmlFile.write("Sorry the ISBN entered in line " + str(n + 1) + "of the ISBN file does not exist")
-            n += 1
-
+            #if the ISBN fails
+            htmlFile.write("<h1>Book " + str(contentNo + 1) + "</h1><br>")
+            htmlFile.write("<p>Sorry the ISBN entered in line " + str(contentNo + 1) + " of the ISBN file does not exist</p><br>")
+            contentNo += 1
+    #close files
     htmlFile.write("</body>")
     htmlFile.close()
     ISBNfile.close()
 except:
-    #create/open file as overwrite
+    #if the ISBN file is missing
     htmlFile = open("ISBNwebpage.html","w")
-    #initial html settings
     htmlFile.write("""
         <!doctype html>
         <html>
@@ -62,63 +85,10 @@ except:
         <meta charset="utf-8">
         <title>ISBN Lookup</title>
         <meta name="viewport" content="width=device-width, initial-scale = 1.0, user-scalable = yes">
-        <link rel="stylesheet" href="005-MathsWebsite.css">
+        <link rel="stylesheet" href="CSS.css">
         </head>
         <body>
-        Sorry the ISBN file does not exist.<br>
-        Please ensure the ISBN.txt file is in the same folder as this script
+        <p>Sorry the ISBN file does not exist.<br>
+        Please ensure the ISBN.txt file is in the same folder as this script</p>
         </body>""")
     htmlFile.close()
-
-#CHECK IF THERE ARE NUMBER
-#CHECK IF NUMBERS ARE VALID
-
-
-# Create CSS file
-#
-#    #create html
-#
-#    #append html
-#htmlContent = """
-#
-#"""
-
-# Open HTML file
-
-
-##"""
-##Found py script to retrieve details from ISBN input, useful information that I'll have to convert to mine
-##
-##import urllib.request
-##import json
-##import textwrap
-##
-##while True:
-##
-##    base_api_link = "https://www.googleapis.com/books/v1/volumes?q=isbn:"
-##    user_input = input("Enter ISBN: ").strip()
-##
-##    with urllib.request.urlopen(base_api_link + user_input) as f:
-##        text = f.read()
-##
-##    decoded_text = text.decode("utf-8")
-##    obj = json.loads(decoded_text) # deserializes decoded_text to a Python object
-##    volume_info = obj["items"][0] 
-##    authors = obj["items"][0]["volumeInfo"]["authors"]
-##
-##    # displays title, summary, author, domain, page count and language
-##    print("\nTitle:", volume_info["volumeInfo"]["title"])
-##    print("\nSummary:\n")
-##    print(textwrap.fill(volume_info["searchInfo"]["textSnippet"], width=65))
-##    print("\nAuthor(s):", ",".join(authors))
-##    print("\nPublic Domain:", volume_info["accessInfo"]["publicDomain"])
-##    print("\nPage count:", volume_info["volumeInfo"]["pageCount"])
-##    print("\nLanguage:", volume_info["volumeInfo"]["language"])
-##    print("\n***")
-##
-##    status_update = input("\nEnter another ISBN? y or n: ").lower().strip()
-##
-##    if status_update == "n":
-##        print("\nThank you! Have a nice day.")
-##        break
-##"""
