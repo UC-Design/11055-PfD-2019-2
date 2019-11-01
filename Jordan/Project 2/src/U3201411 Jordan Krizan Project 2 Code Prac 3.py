d72c9cf9405adf2bbbcf01d625d845f9cf8e339d
jordankrizan <53715194+jordankrizan@users.noreply.github.com>
2019-11-01 23:45:10 +1100

    Create U3201411 Jordan Krizan Project 2 Code Prac 3.py

---
+++ Jordan/Project 2/src/U3201411 Jordan Krizan Project 2 Code Prac 3.py
@@ -0,0 +1,20 @@
+f = open("urlopen.txt", "r")
+nf = open("urllib.html", "w")
+
+arr = []
+
+for line in f:
+    # do something to i
+    arr.append(line)
+    
+for isbn in arr:
+    
+    import link
+    
+    link = "http://covers.openlibrary.org/b/isbn/print(reader.read)-L.jpg"
+    nf = "urllib.html".link
+    myfile = nf.write()
+    print(myfile)
+    
+f.close()
+nf.close()
