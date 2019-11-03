3f1f97b187c73965c2b4b0de196782ab954347b6
justinmathey <53715262+justinmathey@users.noreply.github.com>
2019-11-03 20:47:09 +1100

    Update project2.py

--- Project2/u3190143/project2.py
+++ Project2/u3190143/project2.py
@@ -1,47 +1,50 @@
+
+#   p1 will create and open a html file with write mode were the content will be saved too
 p1 = open("project2.html", "w")
 
+#   Define the structure of the html by storing HTML tags in multiline Python strings   ??
+#   This starts and ends the head and starts the body
 html_start = """<html>
 <head></head>
 <body>"""
-
+#   This ends the body
 html_finish = """</body>
 </html>"""
 
+#   Creates isbn start and finsih variables
+#   Searches the Openlibrary resource for an image
 isbn_start = '<img src="http://covers.openlibrary.org/b/isbn/'
-isbn_finish = '-S.jpg" />'
+#   Presents image in Large jpg format
+isbn_finish = '-L.jpg" />'
+#   Creates variable that holds a error message
 html_error = "Error"
 
-# Program
 
-#   Open isbn file for reading
+#   A variable that opens the isbn.txt file for reading
 isbn_text = open("isbn.txt", "r")
 
-#  Read all the lines into an array
+#   Totallines will read all lines in an array
 totallines = isbn_text.readlines()
-#   Make x equal to the number of lines
+#   X will be equal to the number of lines read by the totallines veriable
 x = len(totallines)
-#   If x (number of lines) is equal to 0 (file has no lines)
-if x == 2:
-#  Print error message 
+#   If x (number of lines) is equal to 0 (file has no lines) Print error message
+if x == 0: 
     p1.write(html_error)
 
-#   Print(totallines)
-
-#write opening html
-
+#   Write the starting html in the p1 file
 p1.write(html_start)
 
-#write all the book stuff
-
+#   Fro each of the lines in the isbn file
 for xlines in totallines:
+#   Write the starting html, the (xlines) then the finishing html in the p1 file
     p1.write(isbn_start + xlines + isbn_finish)
-#    print (xlines)
 
-
-
-# write the final html
-
+#   write the finishing html in the p1 file
 p1.write(html_finish)
 
+
+
+#    Close the isbn file
 isbn_text.close()
+#   Close the p1 file so it cannot be read or written in anymore 
 p1.close()
